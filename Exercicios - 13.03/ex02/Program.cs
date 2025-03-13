156e46c94c7810dc8cf9a8a615d27cc1bdc9919f
LEANDRO SANTOS <leandro.santos06@cs.up.edu.br>
2025-03-13 11:26:48 -0300

    Adicionar projetos de Ambientação e Lista de Exercícios

---
+++ Exercicios - 13.03/ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program {
+    static void Main (string[] args){
+
+        int[] vetor = new int[100];
+
+//Gerar um valor randômico
+Random r = new Random();
+
+//Preencher o vetor com valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(1000);
+}
+
+//Imprimir o vetor com valores aletatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+Array.Sort(vetor);
+
+Console.WriteLine("\n");
+//Imprimir o vetor com valores ordenados
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+    }
+}
